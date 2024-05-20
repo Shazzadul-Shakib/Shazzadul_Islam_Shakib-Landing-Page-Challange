6e73a297acdb4337e00ff94d35727de2f14a1be1
Shazzadul-Shakib <shakib1186@gmail.com>
2024-05-20 12:00:37 +0600

    project initialized and setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./layout/Main"
+
+function App() {
+
+  return (
+    <>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>This is body part</h1>
+    </div>
+  );
+};
+
+export default Main;
